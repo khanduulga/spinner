d9d80e27ed14157e567827793051759294516eda
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2020-12-01 23:42:56 +0000

    spinner2 refactored to spin as many times as you want in while loop

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,13 @@
 let delay = 0;
-const spinners = ['|', '/', '-', '\\','|', '/', '-', '\\','|'];
+const spinners = ['|', '/', '-', '\\','|'];
 
-for (const spin of spinners) {
-  setTimeout(() => {
-    process.stdout.write('\r' + spin);
-  }, delay);
-  delay += 200;
+while (delay < 5000) {
+  for (const spin of spinners) {
+    setTimeout(() => {
+      process.stdout.write('\r' + spin);
+    }, delay);
+    delay += 200;
+  }
 }
 
 setTimeout(() => {
